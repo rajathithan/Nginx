3305465017ff6a791cf48f36c09345547b998a80
Rajathithan Rajasekar <rajathithan@gmail.com>
2019-07-01 11:25:05 -0500

    Nginx health check script

--- Health check script.sh
+++ Health check script.sh
@@ -19,19 +19,19 @@ do
     pa1-down = 0
 
     # update NGINX config
-    sudo sed -i 's/server 192.168.1.1:8007 down/server 192.168.1.1:8007/g' /etc/nginx/sites-available/uat10-vhost
+    sudo sed -i 's/server 192.168.1.1:8007 down/server 192.168.1.1:8007/g' /etc/nginx/sites-available/vhostfile
     if [ $? -eq 0]
     then
-        echo "Successfully marked upstream server 1 as up in Nginx Config" > /tmp/palaltouptimecheck.log
+        echo "Successfully marked upstream server 1 as up in Nginx Config" > /tmp/uptimecheck.log
     fi
 
     # Restart Nginx
     echo "Restarting the nginx service"
     echo "======================================================"
-    sudo service nginx restart > /tmp/palaltouptimecheck.log
+    sudo service nginx restart > /tmp/uptimecheck.log
     echo "Nginx service status"
     echo "======================================================"
-    sudo service nginx status > /tmp/palaltouptimecheck.log
+    sudo service nginx status > /tmp/uptimecheck.log
   else
     # To prevent marking the server as down which is already down
     if [[ "$pa1-down" -ne 1 ]]
@@ -40,19 +40,19 @@ do
         pa1-down = 1
 
         # update NGINX config
-        sudo sed -i 's/server 192.168.1.1:8007/server 192.168.1.1:8007 down/g' /etc/nginx/sites-available/uat10-vhost
+        sudo sed -i 's/server 192.168.1.1:8007/server 192.168.1.1:8007 down/g' /etc/nginx/sites-available/vhostfile
         if [ $? -eq 0]
         then
-            echo "Successfully marked upstream server 1 as down in Nginx Config" > /tmp/palaltouptimecheck.log
+            echo "Successfully marked upstream server 1 as down in Nginx Config" > /tmp/uptimecheck.log
         fi
 
         # Restart Nginx
         echo "Restarting the nginx service"
         echo "======================================================"
-        sudo service nginx restart > /tmp/palaltouptimecheck.log
+        sudo service nginx restart > /tmp/uptimecheck.log
         echo "Nginx service status"
         echo "======================================================"
-        sudo service nginx status > /tmp/palaltouptimecheck.log
+        sudo service nginx status > /tmp/uptimecheck.log
     fi
   fi
 
@@ -64,19 +64,19 @@ do
     pa2-down = 0
 
     # update NGINX config
-    sudo sed -i 's/server 192.168.1.2:8007 down/server 192.168.1.2:8007/g' /etc/nginx/sites-available/uat10-vhost
+    sudo sed -i 's/server 192.168.1.2:8007 down/server 192.168.1.2:8007/g' /etc/nginx/sites-available/vhostfile
     if [ $? -eq 0]
     then
-        echo "Successfully marked upstream server 2 as up in Nginx Config" > /tmp/palaltouptimecheck.log
+        echo "Successfully marked upstream server 2 as up in Nginx Config" > /tmp/uptimecheck.log
     fi
 
     # Restart Nginx
     echo "Restarting the nginx service"
     echo "======================================================"
-    sudo service nginx restart > /tmp/palaltouptimecheck.log
+    sudo service nginx restart > /tmp/uptimecheck.log
     echo "Nginx service status"
     echo "======================================================"
-    sudo service nginx status > /tmp/palaltouptimecheck.log
+    sudo service nginx status > /tmp/uptimecheck.log
   else
     # To prevent marking the server as down which is already down
     if [[ "$pa2-down" -ne 1 ]]
@@ -85,19 +85,19 @@ do
         pa2-down = 1
 
         # update NGINX config
-        sudo sed -i 's/server 192.168.1.2:8007/server 192.168.1.2:8007 down/g' /etc/nginx/sites-available/uat10-vhost
+        sudo sed -i 's/server 192.168.1.2:8007/server 192.168.1.2:8007 down/g' /etc/nginx/sites-available/vhostfile
         if [ $? -eq 0]
         then
-            echo "Successfully marked upstream server 2 as down in Nginx Config" > /tmp/palaltouptimecheck.log
+            echo "Successfully marked upstream server 2 as down in Nginx Config" > /tmp/uptimecheck.log
         fi
 
         # Restart Nginx
         echo "Restarting the nginx service"
         echo "======================================================"
-        sudo service nginx restart > /tmp/palaltouptimecheck.log
+        sudo service nginx restart > /tmp/uptimecheck.log
         echo "Nginx service status"
         echo "======================================================"
-        sudo service nginx status > /tmp/palaltouptimecheck.log
+        sudo service nginx status > /tmp/uptimecheck.log
     fi
   fi
 
